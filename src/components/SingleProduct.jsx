954fa9f803bbf516a401fb38892db5a8c527c331
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-05 18:41:15 -0400

    big annoying layout restucture

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -75,23 +75,23 @@ const singleProduct = () => {
 
   return (
     <>
-      {imageBaseURL && !productLoading && (
-        <>
       <Spinner loading={loading} />
-          <main className='flex justify-center font-raleway text-[#212922] md:h-[470px] 3xl:mt-[4%] 4xl:mx-auto 4xl:min-h-[690px] 4xl:w-[1700px] 6xl:w-[2200px]'>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{
-                delay: 0.3,
-                duration: 0.5,
-                ease: [0.17, 0.67, 0.83, 0.67],
-              }}
-              className='mt-8 flex flex-col justify-center md:flex-row md:gap-20'
-            >
+      {imageBaseURL && !productLoading && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.5,
+            ease: [0.17, 0.67, 0.83, 0.67],
+          }}
+          className='portrait:lg:flex portrait:lg:flex-col portrait:lg:gap-36 '
+        >
+          <main className='flex justify-center font-raleway text-[#212922] md:h-[470px] 3xl:mt-[4%] 4xl:mx-auto 4xl:min-h-[690px] 4xl:w-[1700px] 6xl:w-[2200px] portrait:px-8 portrait:md:mt-10'>
+            <div className='mt-8 flex flex-col justify-center md:flex-row md:gap-20 portrait:md:justify-center portrait:md:w-screen portrait:md:gap-5 portrait:lg:gap-20'>
               {/**mobile header only */}
-              <div className='mx-auto w-fit md:mx-0'>
+              <div className='mx-auto w-fit md:mx-0 portrait:md:w-1/2 portrait:lg:w-fit'>
                 <header className='flex justify-center pb-4 text-center  font-outfit text-[4.9vw] font-medium-light uppercase text-green-gray md:hidden '>
                   {singleProduct.name}
                 </header>
@@ -126,28 +126,28 @@ const singleProduct = () => {
                 </div>
               </div>
               {/**desktop header */}
-              <div className='mx-8 md:mx-0 md:w-1/3'>
+              <div className=' md:mx-0 md:w-1/3 portrait:md:w-1/2 portrait:xs:w-full'>
                 <div className='mb-[5%] hidden flex-col  items-end justify-center md:flex'>
-                  <header className=' self-center pr-6 font-outfit text-[2.3vw] lg:text-[1.7vw] uppercase text-green-gray 3xl:text-[2vw] 4xl:text-[1.5vw]'>
+                  <header className=' self-center pr-6 font-outfit text-[2.3vw] lg:text-[1.7vw] uppercase text-green-gray 3xl:text-[2vw] 4xl:text-[1.5vw] portrait:md:text-[3.5vw]'>
                     {singleProduct.name}
                   </header>
                   <LikedProduct />
                 </div>
 
-                <div className='mb-0 flex justify-between p-2 text-[2.5vw] md:mb-4 md:border-b-4 md:p-0 md:text-[1vw] 3xl:text-[.7vw]'>
+                <div className='mb-0 flex justify-between p-2 text-[2.5vw] md:mb-4 md:border-b-4 md:p-0 md:text-[1vw] 3xl:text-[.7vw] portrait:md:text-[1.5vw]'>
                   <p>
                     {singleProduct?.tags
                       ?.map(({ tagName }) => tagName)
                       .join(', ')}
                   </p>
                 </div>
-                <p className='mb-2 text-[3.7vw] font-bold text-[#212922] md:mb-4 md:text-[2vw] xl:text-[1.6vw] 4xl:text-[1.3vw]'>
+                <p className='mb-2 text-[3.7vw] font-bold text-[#212922] md:mb-4 md:text-[2vw] xl:text-[1.6vw] 4xl:text-[1.3vw] portrait:md:text-[2.7vw]'>
                   ${singleProduct.price}
                 </p>
 
                 {fullDescription && (
                   <p
-                    className='mb-8 min-w-full cursor-pointer text-justify text-[2.8vw] leading-tight md:text-[1vw] 4xl:text-[.7vw]'
+                    className='mb-8 min-w-full portrait:md:text-[1.8vw] cursor-pointer text-justify text-[2.8vw] leading-tight md:text-[1vw] 4xl:text-[.7vw]'
                     onClick={handleFullDescription}
                   >
                     {fullDescription}
@@ -166,7 +166,7 @@ const singleProduct = () => {
                       notify();
                       addToCart();
                     }}
-                    className='btn ease mx-auto block w-full transition-all duration-500 hover:scale-[1.02]  py-2 font-light font-outfit text-[3.8vw] text-white md:text-[2vw] lg:text-[1.7vw] 4xl:text-[1.5vw] 5xl:text-[1.2vw] '
+                    className='btn ease mx-auto block w-full transition-all duration-500 hover:scale-[1.02]  py-2 font-light font-outfit text-[3.8vw] text-white md:text-[2vw] lg:text-[1.7vw] 4xl:text-[1.5vw] 5xl:text-[1.2vw] portrait:md:text-[2.8vw]'
                   >
                     <span>ADD TO CART</span>
                   </button>
@@ -175,17 +175,17 @@ const singleProduct = () => {
                   <img
                     src={box}
                     alt='shipping box icon'
-                    className='w-4 4xl:w-[6]'
+                    className='w-4 4xl:w-[6] portrait:md:w-6'
                   />
-                  <p className='text-[2.5vw] md:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.7vw]'>
+                  <p className='text-[2.5vw] md:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.7vw] portrait:md:text-[1.6vw] portrait:lg:tex-[1.9vw]'>
                     Free shipping in the USA
                   </p>
                 </div>
               </div>
-            </motion.div>
+            </div>
           </main>
           <SimilarProducts />
-        </>
+        </motion.div>
       )}
     </>
   );
