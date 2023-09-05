709fb07037b7b0de28b8b1b031b6063f85ee6507
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-05 15:41:46 -0400

    add framer

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -11,6 +11,8 @@ import plus from '../assets/plus.svg';
 import { Link } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
 
+import {motion} from 'framer-motion'
+
 const CartCard = (props) => {
   const dispatch = useDispatch();
 
@@ -23,66 +25,75 @@ const CartCard = (props) => {
   function incrementCart(productId) {
     dispatch(addOneToCart(productId));
   }
-  function removeFromCart(productId) {
+    function removeFromCart(productId) {
     toast.error('Product removed from cart!');
     dispatch(removeCartRow(productId));
   }
 
   return (
     <>
-      <div className="flex h-52 items-center justify-around gap-6 md:mx-4 portrait:xs:gap-3 portrait:xs:px-1">
-        <div className="h-36 2xl:h-40">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{
+          delay: 0.1,
+          duration: 0.5,
+          ease: [0.17, 0.67, 0.83, 0.67],
+        }}
+        className='flex h-52 items-center justify-around gap-6 md:mx-4 portrait:xs:gap-3 portrait:xs:px-1'
+      >
+        <div className='h-36 2xl:h-40'>
           <img
             src={`${product.imageURL}`}
             alt={`product photo of ${product.name}`}
-            className="h-36 2xl:h-40"
+            className='h-36 2xl:h-40'
           />
         </div>
-        <div className="min-w-48 flex flex-col gap-2  justify-center items-center">
+        <div className='min-w-48 flex flex-col gap-2  justify-center items-center'>
           <Link to={`/products/${product.id}`}>
-            <h1 className="cursor-pointer text-[1.5vw] uppercase hover:underline xl:text-[1.4vw] 2xl:text-[1.2vw] 4xl:text-[1vw] 5xl:text-[.8vw] 6xl:text-[.6vw] portrait:text-center  portrait:xs:text-[3.8vw] portrait:md:text-[2.5vw]">
+            <h1 className='cursor-pointer text-[1.5vw] uppercase hover:underline xl:text-[1.4vw] 2xl:text-[1.2vw] 4xl:text-[1vw] 5xl:text-[.8vw] 6xl:text-[.6vw] portrait:text-center  portrait:xs:text-[3.8vw] portrait:md:text-[2.5vw]'>
               {product.name}
             </h1>
           </Link>
-          <p className="text-center text-[1.2vw] italic text-gray-600 xl:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.6vw] 6xl:text-[.5vw] portrait:text-center portrait:text-[1.8vw] portrait:xs:text-[2.7vw] portrait:md:text-[2vw]">
+          <p className='text-center text-[1.2vw] italic text-gray-600 xl:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.6vw] 6xl:text-[.5vw] portrait:text-center portrait:text-[1.8vw] portrait:xs:text-[2.7vw] portrait:md:text-[2vw]'>
             {product?.tags.map(({ tagName }) => tagName).join(', ')}
           </p>
-          <p className="text-[1.5vw] xl:text-[1.3vw] 2xl:text-[1.1vw] 4xl:text-[1vw] 5xl:text-[.8vw] 6xl:text-[.6vw] portrait:text-center portrait:text-[2.3vw] portrait:xs:text-[3.2vw] portrait:md:text-[2.5vw] ">
+          <p className='text-[1.5vw] xl:text-[1.3vw] 2xl:text-[1.1vw] 4xl:text-[1vw] 5xl:text-[.8vw] 6xl:text-[.6vw] portrait:text-center portrait:text-[2.3vw] portrait:xs:text-[3.2vw] portrait:md:text-[2.5vw] '>
             ${product.price}
           </p>
-          <div className="flex w-fit  gap-2 rounded-full border border-green-gray px-2 py-1 2xl:py-[3px] 4xl:py-[2px] portrait:self-center">
+          <div className='flex w-fit  gap-2 rounded-full border border-green-gray px-2 py-1 2xl:py-[3px] 4xl:py-[2px] portrait:self-center'>
             <button onClick={() => decrementCart(product.id)}>
               <img
                 src={minus}
-                alt="minus qty icon"
-                className="w-2 xl:w-3 4xl:w-4 portrait:xs:w-5"
+                alt='minus qty icon'
+                className='w-2 xl:w-3 4xl:w-4 portrait:xs:w-5'
               />
             </button>
-            <p className="text-[1vw] 4xl:text-[.8vw] 5xl:text-[.6vw] 6xl:text-[.4vw] portrait:xs:text-[3vw] portrait:xs:py-[1px] portrait:md:text-[2vw] portrait:md:px-2">
+            <p className='text-[1vw] 4xl:text-[.8vw] 5xl:text-[.6vw] 6xl:text-[.4vw] portrait:xs:text-[3vw] portrait:xs:py-[1px] portrait:md:text-[2vw] portrait:md:px-2'>
               {item.qty}
             </p>
             <button onClick={() => incrementCart(product.id)}>
               <img
                 src={plus}
-                alt="plus qty icon"
-                className="w-2 xl:w-3 4xl:w-4 portrait:xs:w-5"
+                alt='plus qty icon'
+                className='w-2 xl:w-3 4xl:w-4 portrait:xs:w-5'
               />
             </button>
           </div>
         </div>
-        <div className="ml-8">
+        <div className='ml-8'>
           <button
             onClick={() => {
               removeFromCart(product.id);
             }}
-            className="block   border border-green-gray/50 py-1 px-2 text-[1.2vw] transition-all duration-500 hover:bg-gray-100 3xl:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.6vw] 6xl:text-[.5vw] portrait:text-[2vw] portrait:xs:text-[3.1vw] portrait:md:text-[2.2vw]"
+            className='block   border border-green-gray/50 py-1 px-2 text-[1.2vw] transition-all duration-500 hover:bg-gray-100 3xl:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.6vw] 6xl:text-[.5vw] portrait:text-[2vw] portrait:xs:text-[3.1vw] portrait:md:text-[2.2vw]'
           >
             remove
           </button>
         </div>
-      </div>
-      <div className="mx-auto w-5/6 border-b-2 border-gray-300"></div>
-      {/* <Toaster /> */}
+      </motion.div>
+      <div className='mx-auto w-5/6 border-b-2 border-gray-300'></div>
     </>
   );
 };
