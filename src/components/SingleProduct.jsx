15431d305a3015e1147799e38af14ed6a4e78f1b
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-11 22:07:05 -0400

    add big layout restructure

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -85,13 +85,13 @@ const singleProduct = () => {
             duration: 0.5,
             ease: [0.17, 0.67, 0.83, 0.67],
           }}
-          className='portrait:lg:flex portrait:lg:flex-col portrait:lg:gap-36 '
+          className='portrait:lg:flex portrait:lg:flex-col portrait:lg:gap-16 '
         >
-          <main className='flex justify-center font-raleway text-[#212922] md:h-[470px] 3xl:mt-[4%] 4xl:mx-auto 4xl:min-h-[690px] 4xl:w-[1700px] 6xl:w-[2200px] portrait:px-8 portrait:md:mt-10'>
-            <div className='mt-8 flex flex-col justify-center md:flex-row md:gap-20 portrait:md:justify-center portrait:md:w-screen portrait:md:gap-5 portrait:lg:gap-20'>
+          <main className='flex justify-center font-raleway text-[#212922] md:h-[470px] landscape:3xl:mt-[1%] 4xl:mx-auto 4xl:min-h-[690px] 4xl:w-[1700px] 6xl:w-[2200px] portrait:px-4 portrait:md:mt-10 portrait:pt-16 landscape:pt-16 landscape:lg:pt-24 landscape:xl:pt-36 landscape:5xl:pt-44 portrait:h-[700px] portrait:lg:pt-36 landscape:short:pt-96'>
+            <div className='mt-8 flex flex-col justify-center md:flex-row md:gap-5 portrait:md:w-screen portrait:md:gap-4 portrait:lg:gap-20 portrait:md:mx-7'>
               {/**mobile header only */}
               <div className='mx-auto w-fit md:mx-0 portrait:md:w-1/2 portrait:lg:w-fit'>
-                <header className='flex justify-center pb-4 text-center  font-outfit text-[4.9vw] font-medium-light uppercase text-green-gray md:hidden '>
+                <header className='flex justify-center pb-4 text-center  font-outfit text-[4.9vw] landscape:text-[1.4rem] font-medium-light uppercase text-green-gray md:hidden '>
                   {singleProduct.name}
                 </header>
                 <div className='relative'>
@@ -115,7 +115,7 @@ const singleProduct = () => {
                       height={1400}
                     />
                     <img
-                      className='h-96 w-80 md:h-full 4xl:w-[460px]'
+                      className='h-96 w-80  landscape:md:h-full 4xl:w-[460px]'
                       src={`${singleProduct.imageURL}`}
                       alt='error showing photo'
                       width={1070}
@@ -125,34 +125,34 @@ const singleProduct = () => {
                 </div>
               </div>
               {/**desktop header */}
-              <div className=' md:mx-0 md:w-1/3 portrait:md:w-1/2 portrait:xs:w-full'>
+              <div className=' md:mx-0 md:w-1/3 portrait:md:w-3/6  portrait::w-full landscape:mx-12 '>
                 <div className='mb-[5%] hidden flex-col  items-end justify-center md:flex'>
-                  <header className=' self-center pr-6 font-outfit text-[2.3vw] lg:text-[1.7vw] uppercase text-green-gray 3xl:text-[2vw] 4xl:text-[1.5vw] portrait:md:text-[3.5vw]'>
+                  <header className=' self-center pr-6 font-outfit text-[2.3vw] landscape:lg:text-[1.7vw] uppercase text-green-gray landscape:4xl:text-[1.5vw] portrait:md:text-[3vw] landscape:6xl:text-[2.4rem]'>
                     {singleProduct.name}
                   </header>
                   <LikedProduct />
                 </div>
 
-                <div className='mb-0 flex justify-between p-2 text-[2.5vw] md:mb-4 md:border-b-4 md:p-0 md:text-[1vw] 3xl:text-[.7vw] portrait:md:text-[1.5vw] portrait:xxs:text-[3.3vw]'>
+                <div className='mb-0 flex justify-between p-2 text-[2.5vw] md:mb-4 md:border-b-4 md:p-0  md:text-[1vw] 3xl:text-[.7vw] portrait:md:text-[1.9vw] portrait:text-[3.3vw]'>
                   <p>
                     {singleProduct?.tags
                       ?.map(({ tagName }) => tagName)
                       .join(', ')}
                   </p>
                 </div>
-                <p className='mb-2 text-[3.7vw] font-bold text-[#212922] md:mb-4 md:text-[2vw] portrait:xxs:text-[6vw] xl:text-[1.6vw] 4xl:text-[1.3vw] portrait:md:text-[2.7vw] portrait:xs:text-[4.9vw]'>
+                <p className='mb-2 text-[3.7vw] font-bold text-[#212922] md:mb-4 md:text-[2vw]  xl:text-[1.6vw] 4xl:text-[1.3vw] portrait:md:text-[1.5rem]  portrait:lg:text-[1.8rem] portrait:text-[4.9vw]'>
                   ${singleProduct.price}
                 </p>
 
                 {fullDescription && (
                   <p
-                    className='mb-8 min-w-full portrait:md:text-[1.8vw] cursor-pointer text-justify text-[2.8vw] leading-tight md:text-[1vw] 4xl:text-[.7vw] portrait:xs:text-[3.5vw] portrait:xxs:text-[4vw]'
+                    className='mb-8 min-w-full  cursor-pointer text-justify text-[2.8vw] leading-tight md:text-[1vw] 4xl:text-[.7vw] portrait:text-[3.5vw]  portrait:md:text-[1rem] portrait:lg:text-[1.1rem]'
                     onClick={handleFullDescription}
                   >
                     {fullDescription}
 
                     {fullDescription === singleProduct.shortDescription ? (
-                      <span className='font-bold'> see more</span>
+                      <span className='font-bold '> see more</span>
                     ) : (
                       <span className='font-bold'> see less</span>
                     )}
@@ -165,7 +165,7 @@ const singleProduct = () => {
                       notify();
                       addToCart();
                     }}
-                    className='btn ease mx-auto block w-full transition-all duration-500 hover:scale-[1.02]  py-2 font-light font-outfit text-[3.8vw] text-white md:text-[2vw] lg:text-[1.7vw] 4xl:text-[1.5vw] 5xl:text-[1.2vw] portrait:md:text-[2.8vw] portrait:xxs:text-[5vw]'
+                    className='btn ease mx-auto block w-full transition-all duration-500 hover:scale-[1.02]  py-2 font-light font-outfit text-[3.8vw] text-white md:text-[2vw] landscape:lg:text-[1.7vw] landscape:4xl:text-[1.5vw] landscape:5xl:text-[1.2vw] portrait:md:text-[2.1vw] portrait:text-[5vw] landscape:6xl:text-[2.2rem]'
                   >
                     <span>ADD TO CART</span>
                   </button>
@@ -176,7 +176,7 @@ const singleProduct = () => {
                     alt='shipping box icon'
                     className='w-4 4xl:w-[6] portrait:md:w-6'
                   />
-                  <p className='text-[2.5vw] md:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.7vw] portrait:md:text-[1.6vw] portrait:lg:tex-[1.9vw] portrait:xxs:text-[3.7vw]'>
+                  <p className='text-[2.5vw] md:text-[1vw] 4xl:text-[.8vw] 5xl:text-[.7vw] portrait:md:text-[1.7vw] portrait:lg:tex-[1.9vw] portrait:text-[3.7vw]'>
                     Free shipping in the USA
                   </p>
                 </div>
