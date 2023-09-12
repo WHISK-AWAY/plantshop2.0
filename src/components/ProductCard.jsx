143f92c1cc028f1fe59460adeccce692ae87b0cc
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-11 22:06:34 -0400

    add landscape bkpts

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -30,7 +30,7 @@ const ProductCard = (props) => {
             to={`/products/${product.id}`}
           >
             <div
-              className={` w-full h-full`}
+              className={` w-full h-full landscape:xs:w-56 landscape:sm:w-72`}
               style={{
                 backgroundImage: `url(${imageBaseURL}-lq_1.webp)`,
                 backgroundRepeat: 'no-repeat',
@@ -40,7 +40,7 @@ const ProductCard = (props) => {
             >
               <picture
                 id={product.id}
-                className='group relative w-full product-picture-wrapper'
+                className='group relative w-full  product-picture-wrapper'
                 onLoad={() => reportIn !== undefined && reportIn(product.id)}
               >
                 {/* Product images are a few pixels off of given h/w below, but this is good enough for preventing layout shift */}
@@ -69,16 +69,16 @@ const ProductCard = (props) => {
           </Link>
           <button
             onClick={addToCart}
-            className='ease bottom-0 mx-auto flex w-full justify-center bg-green-gray py-1  font-medium text-white opacity-80 transition duration-500 hover:opacity-100 group-hover:visible md:invisible md:absolute md:opacity-60 portrait:visible'
+            className='ease bottom-0 mx-auto flex w-full justify-center bg-green-gray py-1  font-medium text-white opacity-80 transition duration-500 hover:opacity-100 group-hover:visible lg:invisible md:absolute md:opacity-60 portrait:visible'
           >
             ADD TO CART
           </button>
         </div>
         <Link to={`/products/${product.id}`}>
-          <p className='pt-3 portrait:xs:pt-1 portrait:md:pt-3 text-center text-[3.9vw] font-medium-light uppercase md:mb-1 md:text-[1.3vw] 5xl:text-[.8vw] lg:text-[1.2vw] 3xl:text-[1vw] 6xl:text-[.7vw] portrait:xs:text-[4vw] portrait:md:text-[2.2vw] portrait:lg:text-[2vw]'>
+          <p className='pt-3 portrait:xs:pt-1 landscape:xs:w-56 landscape:sm:w-72 portrait:md:pt-3 text-center landscape:text-[1rem] font-medium-light uppercase md:mb-1 md:text-[1.3vw] 5xl:text-[.8vw] lg:text-[1.2vw] 3xl:text-[1vw] 6xl:text-[.7vw] portrait:text-[4vw] portrait:md:text-[1.1rem] portrait:lg:text-[2vw]'>
             {product.name}
           </p>
-          <p className='text-center text-[3.7vw] md:text-[1.3vw] 3xl:text-[.9vw] 6xl:text-[.7vw] portrait:xs:text-[4vw]  portrait:md:text-[2.3vw] 5xl:text-[.4]'>
+          <p className='text-center landscape:text-[.9rem] md:text-[1.3vw] landscape:3xl:text-[1rem] 6xl:text-[.7vw] portrait:text-[4vw]  portrait:md:text-[2vw] 5xl:text-[.4]'>
             ${product.price}
           </p>
         </Link>
